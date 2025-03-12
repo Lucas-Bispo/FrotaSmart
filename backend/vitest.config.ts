998ff5507011b6c6d0be1808ec3fbe560bccb256
Lucas-Bispo <lukaobispo@gmail.com>
2025-03-12 20:19:15 -0300

    :triangular_flag_on_post: Erro vitest.config.ts e execução do Vitest UI

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,14 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-    },
-    globals: true,
     ui: true,
-  },
-  server: {
-    port: 51205, // Configura a porta do servidor UI aqui
   },
 });
