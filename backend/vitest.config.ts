e0644b0abd4457e008dd4633f9da227339d005f2
Lucas-Bispo <lukaobispo@gmail.com>
2025-03-12 20:34:21 -0300

    :adhesive_bandage: Adição de schema.prisma, prisma.ts e testes de autenticação

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    ui: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
+    globals: true,
   },
 });
